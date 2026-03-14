ce5257284b1ce0f0fbab32857f09404678f0f17d
jane-nyambura2 <geinramein43@gmail.com>
2026-03-14 15:30:59 +0300

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,12 @@
+def addtwonumber():
+    num1=int(input("Enter first number:"))
+    num2=int(input("Enter second number:"))
+    print(f"The  sum of{num1} and {num2} is {num1+num2}")
+
+
+addtwonumber()
+def subtwonumber():
+    num1=int(input("Enter first number:"))
+    num2=int(input("Enter second number:"))
+    print(f" The difference of {num1} and {num2} is {num1-num2}")
+    
